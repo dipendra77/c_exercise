9793aae902d20b45c274ea0004d1ef6d39884e6b
Dipendra Kumar Ram <Dipendra@Reetus-MacBook-Air.local>
2023-01-18 19:53:25 +0530

    Added program to display no. of  occurences of character in a string: I/P: aabbcd , O/P: 2a2b1c1d

---
+++ string_manipulations/no_of_occurence_str.c
@@ -0,0 +1,52 @@
+/*
+Program to display no. of occurencer of a character in a string.
+Eg: input String: aabbccde
+    Output String: 2a2b2c1d1e 
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void char_occurence_number();
+
+int main(){
+char_occurence_number();	
+return 0;
+
+}
+
+
+void char_occurence_number(){
+
+char a[]= "ccaaabbcd";
+int size=sizeof(a);
+char out[size];
+int count=1;
+int id=0;
+
+for(int i=0;a[i]!='\0';i++)
+{
+	if(a[i]==a[i+1])
+	{
+	count++;
+// 	printf("Count is : %d\n",count);
+	
+	}
+	else
+	{
+		printf("Count of : %c : %d \n",a[i],count);
+		
+		sprintf(out+id,"%d", count);
+		id++;
+		out[id] = a[i];
+		id++;
+		count=1;
+
+	}
+}
+
+
+printf("%s",out);
+
+}
